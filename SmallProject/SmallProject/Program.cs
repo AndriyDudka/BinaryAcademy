c770d4962b911b6d655ba5e1325ea7be8b5a495d
Dudka <andriy.dudka@gmail.com>
2016-05-25 13:04:38 +0300

    create class MyMath with two methods

--- SmallProject/SmallProject/Program.cs
+++ SmallProject/SmallProject/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace SmallProject
 {
+    class MyMath
+    {
+        public long Factorial(int n)
+        {
+            return n < 1 ? 1 : n * Factorial(n - 1);
+        }
+
+        public long Power(int a, int b)
+        {
+            long pow = 1;
+            for (int i = 0; i < b; i++)
+                pow *= a;
+            return pow;
+        }     
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            MyMath myMath = new MyMath();
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(myMath.Factorial(n));
+            Console.WriteLine(myMath.Power(a, b));
         }
     }
 }
